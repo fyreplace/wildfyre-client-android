2d971e87e09111808744fbca1bcae150a06120d0
Laurent Tréguier <laurent@treguier.org>
2019-05-14 10:40:43 +0200

    Use template literals

--- app/src/main/java/net/wildfyre/client/Constants.kt
+++ app/src/main/java/net/wildfyre/client/Constants.kt
@@ -39,11 +39,11 @@ object Constants {
      */
     object Links {
         private const val MAIN_ADDRESS = "https://wildfyre.net/"
-        const val ABOUT_US = MAIN_ADDRESS + "about-us"
-        const val OPEN_SOURCE = MAIN_ADDRESS + "open-source"
-        const val FAQ = MAIN_ADDRESS + "frequently-asked-questions"
-        const val TERMS_AND_CONDITIONS = MAIN_ADDRESS + "terms-and-conditions"
-        const val PRIVACY_POLICY = MAIN_ADDRESS + "privacy-policy"
+        const val ABOUT_US = "${MAIN_ADDRESS}about-us"
+        const val OPEN_SOURCE = "${MAIN_ADDRESS}open-source"
+        const val FAQ = "${MAIN_ADDRESS}frequently-asked-questions"
+        const val TERMS_AND_CONDITIONS = "${MAIN_ADDRESS}terms-and-conditions"
+        const val PRIVACY_POLICY = "${MAIN_ADDRESS}privacy-policy"
         const val FACEBOOK = "https://www.facebook.com/wildfyreapp/"
         const val TWITTER = "https://twitter.com/wildfyreapp"
         const val TELEGRAM = "https://t.me/WildFyreApp"
