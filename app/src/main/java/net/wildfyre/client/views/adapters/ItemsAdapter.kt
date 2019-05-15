08a89e6c9d0c81a8b2e509c3b1fa0a3bcb30fe41
Laurent Tréguier <laurent@treguier.org>
2019-05-15 15:52:55 +0200

    Code clean-up

--- app/src/main/java/net/wildfyre/client/views/adapters/ItemsAdapter.kt
+++ app/src/main/java/net/wildfyre/client/views/adapters/ItemsAdapter.kt
@@ -82,7 +82,7 @@ abstract class ItemsAdapter<I>(private val showAuthors: Boolean) : RecyclerView.
                     CenterCrop(),
                     RoundedCorners(
                         holder.itemView.resources
-                            .getDimension(R.dimen.post_author_picture_rounding)
+                            .getDimension(R.dimen.list_item_author_picture_rounding)
                             .toInt()
                     )
                 )
