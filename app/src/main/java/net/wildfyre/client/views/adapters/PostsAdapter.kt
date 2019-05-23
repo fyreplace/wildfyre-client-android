b896410a256090d4640858e19c8f119d54d5b781
Laurent Tréguier <laurent@treguier.org>
2019-05-23 14:40:34 +0200

    Use pretty date and time format

--- app/src/main/java/net/wildfyre/client/views/adapters/PostsAdapter.kt
+++ app/src/main/java/net/wildfyre/client/views/adapters/PostsAdapter.kt
@@ -2,18 +2,21 @@ package net.wildfyre.client.views.adapters
 
 import net.wildfyre.client.data.Author
 import net.wildfyre.client.data.Post
+import java.text.SimpleDateFormat
 
 /**
  * Adapter for displaying posts with [net.wildfyre.client.views.PostsFragment] implementations.
  */
 open class PostsAdapter(showAuthors: Boolean) : ItemsAdapter<Post>(showAuthors) {
+    private val dateFormat = SimpleDateFormat.getDateTimeInstance()
+
     override fun getText(position: Int): String? = data[position].text
 
     override fun getImage(position: Int): String? = data[position].image
 
     override fun getAuthor(position: Int): Author? = data[position].author
 
-    override fun getSubtitle(position: Int): String = data[position].created.toString()
+    override fun getSubtitle(position: Int): String = dateFormat.format(data[position].created)
 
     override fun getId(position: Int): Long = data[position].id ?: -1
 }
