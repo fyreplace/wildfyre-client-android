ad1981c994ece897002380d67db1c7f869c79e69
Laurent Tréguier <laurent@treguier.org>
2019-05-26 20:04:36 +0200

    Only send errors once

--- app/src/main/java/net/wildfyre/client/viewmodels/FailureHandlingViewModel.kt
+++ app/src/main/java/net/wildfyre/client/viewmodels/FailureHandlingViewModel.kt
@@ -4,16 +4,16 @@ import android.app.Application
 import androidx.annotation.CallSuper
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import net.wildfyre.client.data.Failure
 import net.wildfyre.client.data.FailureHandler
+import net.wildfyre.client.data.SingleLiveEvent
 
 /**
  * Base [androidx.lifecycle.ViewModel] class to use with [net.wildfyre.client.views.FailureHandlingActivity] or
  * [net.wildfyre.client.views.FailureHandlingFragment] that implements basic error propagation.
  */
 abstract class FailureHandlingViewModel(application: Application) : AndroidViewModel(application), FailureHandler {
-    private var _lastFailure = MutableLiveData<Failure>()
+    private var _lastFailure = SingleLiveEvent<Failure>()
 
     val lastFailure: LiveData<Failure>
         get() = _lastFailure
