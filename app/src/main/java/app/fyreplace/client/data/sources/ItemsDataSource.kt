c4490152869c16a0a4a41fa858532648ddc741a6
Laurent Tréguier <laurent@treguier.org>
2019-07-27 16:35:03 +0200

    Let type inference simplify everything

--- app/src/main/java/app/fyreplace/client/data/sources/ItemsDataSource.kt
+++ app/src/main/java/app/fyreplace/client/data/sources/ItemsDataSource.kt
@@ -10,7 +10,7 @@ abstract class ItemsDataSource<I>(private val listener: DataLoadingListener) : P
 
     final override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<I>) {
         listener.onLoadingStart()
-        val count = runFetcher(0, 1)?.count ?: 0
+        val count = runFetcher(0, 1).count
         val initialPosition = computeInitialLoadPosition(params, count)
         val initialSize = computeInitialLoadSize(params, initialPosition, count)
         callback.onResult(loadRange(initialPosition, initialSize), initialPosition, count)
@@ -20,15 +20,11 @@ abstract class ItemsDataSource<I>(private val listener: DataLoadingListener) : P
     final override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<I>) =
         callback.onResult(loadRange(params.startPosition, params.loadSize))
 
-    final override fun invalidate() {
-        super.invalidate()
-        cancel()
-    }
+    final override fun invalidate() = super.invalidate().also { cancel() }
 
-    private fun loadRange(position: Int, size: Int): List<I> = runFetcher(position, size)?.results.orEmpty()
+    private fun loadRange(position: Int, size: Int) = runFetcher(position, size).results
 
-    private fun runFetcher(offset: Int, size: Int): SuperItem<I>? =
-        runBlocking { return@runBlocking runFetchImpl(offset, size) }
+    private fun runFetcher(offset: Int, size: Int) = runBlocking { runFetchImpl(offset, size) }
 
     private suspend fun runFetchImpl(offset: Int, size: Int): SuperItem<I> {
         while (true) {
