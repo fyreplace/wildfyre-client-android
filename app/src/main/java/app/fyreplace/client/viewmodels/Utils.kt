fd060ebc260b18d560034c52809c250fdfc11ca1
Laurent Tréguier <laurent@treguier.org>
2019-08-17 13:23:27 +0200

    Remove deprecated ViewModelProviders usage

--- app/src/main/java/app/fyreplace/client/viewmodels/Utils.kt
+++ app/src/main/java/app/fyreplace/client/viewmodels/Utils.kt
@@ -3,13 +3,13 @@ package app.fyreplace.client.viewmodels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 
 inline fun <reified VM : ViewModel> FragmentActivity.lazyViewModel() =
-    lazy { ViewModelProviders.of(this).get(VM::class.java) }
+    lazy { ViewModelProvider(this).get(VM::class.java) }
 
 inline fun <reified VM : ViewModel> Fragment.lazyViewModel() =
-    lazy { ViewModelProviders.of(this).get(VM::class.java) }
+    lazy { ViewModelProvider(this).get(VM::class.java) }
 
 inline fun <reified VM : ViewModel> Fragment.lazyActivityViewModel() =
-    lazy { ViewModelProviders.of(requireActivity()).get(VM::class.java) }
+    lazy { ViewModelProvider(requireActivity()).get(VM::class.java) }
