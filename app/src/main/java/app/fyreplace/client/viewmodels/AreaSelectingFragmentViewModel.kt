c6d274bfe8b556087e3ab819d47c567f35ce113d
Laurent Tréguier <laurent@treguier.org>
2019-08-24 16:44:27 +0200

    Attempt to fix preferred area randomly changing

--- app/src/main/java/app/fyreplace/client/viewmodels/AreaSelectingFragmentViewModel.kt
+++ app/src/main/java/app/fyreplace/client/viewmodels/AreaSelectingFragmentViewModel.kt
@@ -9,26 +9,21 @@ class AreaSelectingFragmentViewModel : ViewModel() {
     private val mAreas = MutableLiveData<List<Area>>()
     private val mPreferredAreaName = MutableLiveData<String>()
     private val mPreferredArea = MediatorLiveData<Area?>()
-    private val mPreferredAreaIndex = MediatorLiveData<Int?>()
     private val mPreferredAreaReputationInfo = MutableLiveData<Reputation>()
 
     val areas: LiveData<List<Area>> = mAreas.distinctUntilChanged()
     val areasDisplayNames: LiveData<List<String>> = areas.map { it.map { a -> a.displayName } }
     val preferredAreaName: LiveData<String> = mPreferredAreaName.distinctUntilChanged()
     val preferredArea: LiveData<Area?> = mPreferredArea.distinctUntilChanged()
-    val preferredAreaIndex: LiveData<Int?> = mPreferredAreaIndex.distinctUntilChanged()
+    val preferredAreaIndex: LiveData<Int?> = areas.map { areas ->
+        areas.indexOfFirst { it.name == AreaRepository.preferredAreaName }.takeIf { it > -1 }
+    }.distinctUntilChanged()
     val currentAreaSpread: LiveData<Int> = mPreferredAreaReputationInfo.map { it.spread }
     val currentAreaReputation: LiveData<Int> = mPreferredAreaReputationInfo.map { it.reputation }
 
     init {
         mPreferredArea.addSource(areas) { updatePreferredArea(it, preferredAreaName.value) }
         mPreferredArea.addSource(preferredAreaName) { updatePreferredArea(areas.value, it) }
-        mPreferredAreaIndex.addSource(areas) {
-            updatePreferredAreaIndex(it, preferredAreaName.value)
-        }
-        mPreferredAreaIndex.addSource(preferredAreaName) {
-            updatePreferredAreaIndex(areas.value, it)
-        }
     }
 
     suspend fun updateAreas() {
@@ -55,7 +50,4 @@ class AreaSelectingFragmentViewModel : ViewModel() {
 
     private fun updatePreferredArea(areas: List<Area>?, areaName: String?) =
         areas?.firstOrNull { it.name == areaName }?.let { mPreferredArea.postValue(it) }
-
-    private fun updatePreferredAreaIndex(areas: List<Area>?, areaName: String?) =
-        areas?.indexOfFirst { it.name == areaName }?.let { mPreferredAreaIndex.postValue(it) }
 }
