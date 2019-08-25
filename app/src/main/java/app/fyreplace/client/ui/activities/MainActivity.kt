2ce1dfaa2832bff6cb905d8f33627272829d73fe
Laurent Tréguier <laurent@treguier.org>
2019-08-25 22:02:00 +0200

    Let navigation controller handle deep links

--- app/src/main/java/app/fyreplace/client/ui/activities/MainActivity.kt
+++ app/src/main/java/app/fyreplace/client/ui/activities/MainActivity.kt
@@ -184,17 +184,8 @@ class MainActivity : FailureHandlingActivity(), NavController.OnDestinationChang
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
 
-        if (intent == null) {
-            return
-        }
-
-        val uri = intent.data?.path.orEmpty()
-
-        for (pair in REGEX_TO_DIRECTIONS) {
-            pair.key.matchEntire(uri)?.let {
-                findNavController(R.id.navigation_host).navigate(pair.value(it))
-                return
-            }
+        if (intent != null) {
+            findNavController(R.id.navigation_host).handleDeepLink(intent)
         }
     }
 
@@ -404,26 +395,6 @@ class MainActivity : FailureHandlingActivity(), NavController.OnDestinationChang
             R.id.fragment_post,
             R.id.fragment_user,
             R.id.fragment_draft
-        )
-
-        val POST_REGEX = Regex("/areas/(\\w+)/(\\d+)(?:/(\\d+))?")
-        val USER_REGEX = Regex("/user/(\\d+)")
-        val REGEX_TO_DIRECTIONS = mapOf(
-            POST_REGEX to { result: MatchResult ->
-                NavigationMainDirections.actionGlobalFragmentPost(
-                    areaName = result.groupValues[1],
-                    postId = result.groupValues[2].toLong(),
-                    selectedCommentId = result.groupValues[3]
-                        .takeIf { it.isNotEmpty() }
-                        ?.toLong()
-                        ?: -1
-                )
-            },
-            USER_REGEX to { result: MatchResult ->
-                NavigationMainDirections.actionGlobalFragmentUser(
-                    userId = result.groupValues[1].toLong()
-                )
-            }
         )
 
         val IMAGE_TRANSITION = DrawableTransitionOptions.withCrossFade()
