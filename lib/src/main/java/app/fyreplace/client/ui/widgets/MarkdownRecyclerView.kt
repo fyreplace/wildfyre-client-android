af382ed546eada57ab35d2ba9fc0040c861590c5
Laurent Tréguier <laurent@treguier.org>
2019-12-04 19:51:58 +0100

    Fix flickering images

--- lib/src/main/java/app/fyreplace/client/ui/widgets/MarkdownRecyclerView.kt
+++ lib/src/main/java/app/fyreplace/client/ui/widgets/MarkdownRecyclerView.kt
@@ -4,18 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.TextView
-import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
-import app.fyreplace.client.lib.R
 import io.noties.markwon.image.AsyncDrawableScheduler
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
-class MarkdownRecyclerView : RecyclerView, CoroutineScope {
-    private lateinit var mCoroutineContext: CoroutineContext
-    override val coroutineContext: CoroutineContext
-        get() = mCoroutineContext
-
+class MarkdownRecyclerView : RecyclerView {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
@@ -24,28 +16,19 @@ class MarkdownRecyclerView : RecyclerView, CoroutineScope {
         defStyleAttr
     )
 
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        mCoroutineContext = SupervisorJob() + Dispatchers.Main
-        launch {
-            delay(REFRESH_DELAY_INITIAL)
+    override fun onChildAttachedToWindow(child: View) {
+        super.onChildAttachedToWindow(child)
 
-            while (true) {
-                children.map { extractText(it) }
-                    .forEach { it?.post { AsyncDrawableScheduler.schedule(it) } }
-                delay(REFRESH_DELAY)
-            }
+        if (child is TextView) {
+            AsyncDrawableScheduler.schedule(child)
         }
     }
 
-    override fun onDetachedFromWindow() = mCoroutineContext.cancel()
-        .also { super.onDetachedFromWindow() }
+    override fun onChildDetachedFromWindow(child: View) {
+        super.onChildDetachedFromWindow(child)
 
-    private fun extractText(view: View) = view as? TextView
-        ?: view.findViewById(R.id.text) as? TextView
-
-    private companion object {
-        const val REFRESH_DELAY_INITIAL = 1000L
-        const val REFRESH_DELAY = 5000L
+        if (child is TextView) {
+            AsyncDrawableScheduler.unschedule(child)
+        }
     }
 }
