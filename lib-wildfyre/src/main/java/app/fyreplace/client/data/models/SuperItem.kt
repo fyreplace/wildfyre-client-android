82437536e2edb92d0b3a9e153f330191696d730f
Laurent Tréguier <laurent@treguier.org>
2019-12-18 23:16:07 +0100

    Simplify SuperItem

--- lib-wildfyre/src/main/java/app/fyreplace/client/data/models/SuperItem.kt
+++ lib-wildfyre/src/main/java/app/fyreplace/client/data/models/SuperItem.kt
@@ -3,76 +3,36 @@ package app.fyreplace.client.data.models
 import android.os.Parcel
 import android.os.Parcelable
 
-abstract class SuperItem<T : Parcelable>(
+@Suppress("UNCHECKED_CAST")
+open class SuperItem<T : Parcelable>(
     val count: Int,
     val next: String? = null,
     val previous: String? = null,
     val results: List<T>
 ) : Model {
+    private constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(List::class.java.classLoader) as List<T>
+    )
+
     final override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(count)
         parcel.writeString(next)
         parcel.writeString(previous)
-        parcel.writeTypedList(results)
+        parcel.writeValue(results)
+    }
+
+    companion object CREATOR : Parcelable.Creator<SuperItem<*>> {
+        override fun createFromParcel(parcel: Parcel) = SuperItem<Parcelable>(parcel)
+
+        override fun newArray(size: Int) = arrayOfNulls<SuperItem<*>>(size)
     }
 }
 
-class SuperBan(
-    count: Int,
-    next: String?,
-    previous: String?,
-    results: List<Ban>
-) : SuperItem<Ban>(count, next, previous, results) {
-    private constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createTypedArrayList(Ban)!!
-    )
+typealias SuperBan = SuperItem<Ban>
 
-    companion object CREATOR : Parcelable.Creator<SuperBan> {
-        override fun createFromParcel(parcel: Parcel) = SuperBan(parcel)
+typealias SuperNotification = SuperItem<Notification>
 
-        override fun newArray(size: Int) = arrayOfNulls<SuperBan>(size)
-    }
-}
-
-class SuperNotification(
-    count: Int,
-    next: String?,
-    previous: String?,
-    results: List<Notification>
-) : SuperItem<Notification>(count, next, previous, results) {
-    private constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createTypedArrayList(Notification)!!
-    )
-
-    companion object CREATOR : Parcelable.Creator<SuperNotification> {
-        override fun createFromParcel(parcel: Parcel) = SuperNotification(parcel)
-
-        override fun newArray(size: Int) = arrayOfNulls<SuperNotification>(size)
-    }
-}
-
-class SuperPost(
-    count: Int,
-    next: String?,
-    previous: String?,
-    results: List<Post>
-) : SuperItem<Post>(count, next, previous, results) {
-    private constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createTypedArrayList(Post)!!
-    )
-
-    companion object CREATOR : Parcelable.Creator<SuperPost> {
-        override fun createFromParcel(parcel: Parcel) = SuperPost(parcel)
-
-        override fun newArray(size: Int) = arrayOfNulls<SuperPost>(size)
-    }
-}
+typealias SuperPost = SuperItem<Post>
