6a8e6fda19444c687f4d3b535db44d0db185fe0e
Laurent Tréguier <laurent@treguier.org>
2020-01-13 20:59:37 +0100

    Fix crash when loading items

--- lib-items-list/src/main/java/app/fyreplace/client/data/sources/ItemsDataSource.kt
+++ lib-items-list/src/main/java/app/fyreplace/client/data/sources/ItemsDataSource.kt
@@ -12,28 +12,37 @@ abstract class ItemsDataSource<I : Model>(private val listener: DataLoadingListe
 ) {
     protected abstract val fetcher: suspend (Int, Int) -> SuperItem<I>
 
-    final override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<I>) {
-        listener.onLoadingStart()
-        val count = runFetcher(0, 1).count
-        val initialPosition = computeInitialLoadPosition(params, count)
-        val initialSize = computeInitialLoadSize(params, initialPosition, count)
-        val fetch = runFetcher(initialPosition, initialSize)
-        val actualInitialPosition = if (initialPosition < fetch.count) initialPosition else 0
-        callback.onResult(fetch.results, actualInitialPosition, fetch.count)
-        listener.onLoadingStop()
+    final override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<I?>) {
+        fun validateFetch(fetch: SuperItem<I>?) =
+            fetch ?: callback.onResult(emptyList(), 0, 0).let { null }
+
+        try {
+            listener.onLoadingStart()
+            val countingFetch = validateFetch(runFetcher(0, 1)) ?: return
+            val count = countingFetch.count
+            val initialPosition = computeInitialLoadPosition(params, count)
+            val initialSize = computeInitialLoadSize(params, initialPosition, count)
+            val fetch = validateFetch(runFetcher(initialPosition, initialSize)) ?: return
+            val actualInitialPosition = if (initialPosition < fetch.count) initialPosition else 0
+            callback.onResult(fetch.results, actualInitialPosition, fetch.count)
+        } finally {
+            listener.onLoadingStop()
+        }
     }
 
-    final override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<I>) =
-        callback.onResult(runFetcher(params.startPosition, params.loadSize).results)
+    final override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<I?>) {
+        val fetch = runFetcher(params.startPosition, params.loadSize)
+        callback.onResult(fetch?.results ?: List(params.loadSize) { null })
+    }
 
     final override fun invalidate() {
         super.invalidate()
         cancel()
     }
 
-    private fun runFetcher(offset: Int, size: Int): SuperItem<I> = try {
+    private fun runFetcher(offset: Int, size: Int): SuperItem<I>? = try {
         runBlocking(coroutineContext) { fetcher(offset, size) }
     } catch (e: Exception) {
-        SuperItem(0, null, null, emptyList())
+        null
     }
 }
