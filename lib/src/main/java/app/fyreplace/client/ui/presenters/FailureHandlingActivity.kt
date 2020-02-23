95a0de6ebab367ea7d1c939b68ffcb6a72ff5bfe
Laurent Tréguier <laurent@treguier.org>
2020-02-23 20:21:57 +0100

    Remove redundant cancellations

--- lib/src/main/java/app/fyreplace/client/ui/presenters/FailureHandlingActivity.kt
+++ lib/src/main/java/app/fyreplace/client/ui/presenters/FailureHandlingActivity.kt
@@ -1,17 +1,11 @@
 package app.fyreplace.client.ui.presenters
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import app.fyreplace.client.ui.FailureHandler
 import app.fyreplace.client.ui.Presenter
-import kotlinx.coroutines.cancel
 
 abstract class FailureHandlingActivity(contentLayoutId: Int) : AppCompatActivity(contentLayoutId),
     FailureHandler, Presenter {
-    override fun onDestroy() {
-        lifecycleScope.cancel()
-        super.onDestroy()
-    }
 
     override fun getContext() = this
 }
--- lib/src/main/java/app/fyreplace/client/ui/presenters/FailureHandlingFragment.kt
+++ lib/src/main/java/app/fyreplace/client/ui/presenters/FailureHandlingFragment.kt
@@ -1,13 +1,9 @@
 package app.fyreplace.client.ui.presenters
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import app.fyreplace.client.ui.FailureHandler
 import app.fyreplace.client.ui.Presenter
-import kotlinx.coroutines.cancel
 
 abstract class FailureHandlingFragment(contentLayoutId: Int) : Fragment(contentLayoutId),
     FailureHandler, Presenter {
@@ -15,11 +11,4 @@ abstract class FailureHandlingFragment(contentLayoutId: Int) : Fragment(contentL
         super.onAttach(context)
         setHasOptionsMenu(true)
     }
-
-    override fun onDestroy() {
-        lifecycleScope.cancel()
-        super.onDestroy()
-    }
-
-    fun navigate(uri: Uri) = startActivity(Intent(Intent.ACTION_VIEW, uri))
 }
