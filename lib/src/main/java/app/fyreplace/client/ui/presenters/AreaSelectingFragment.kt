03d7f4f248b33d21c94eb80f0283853f7b4cfd7b
Laurent Tréguier <laurent@treguier.org>
2020-04-02 21:48:26 +0200

    Indicate which area is selected when choosing one

--- lib/src/main/java/app/fyreplace/client/ui/presenters/AreaSelectingFragment.kt
+++ lib/src/main/java/app/fyreplace/client/ui/presenters/AreaSelectingFragment.kt
@@ -1,9 +1,13 @@
 package app.fyreplace.client.ui.presenters
 
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuInflater
+import androidx.core.text.set
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import app.fyreplace.client.data.models.Area
 import app.fyreplace.client.lib.R
 import app.fyreplace.client.lib.databinding.ActionAreaSelectingSelectorBinding
 import app.fyreplace.client.ui.FailureHandler
@@ -16,34 +20,45 @@ import org.koin.androidx.viewmodel.ext.android.getSharedViewModel
  */
 interface AreaSelectingFragment : FailureHandler {
     fun onCreateOptionsMenu(fragment: Fragment, menu: Menu, inflater: MenuInflater) {
-        val areaSelectingViewModel = fragment.getSharedViewModel<AreaSelectingFragmentViewModel>()
+        val viewModel = fragment.getSharedViewModel<AreaSelectingFragmentViewModel>()
         val areaSelectorMenuItem = menu.findItem(R.id.action_area_selector)
         val bd = ActionAreaSelectingSelectorBinding
             .bind(areaSelectorMenuItem.actionView)
             .apply {
                 lifecycleOwner = fragment.viewLifecycleOwner
-                model = areaSelectingViewModel
+                model = viewModel
             }
 
-        areaSelectingViewModel.preferredArea.observe(fragment.viewLifecycleOwner) {
+        viewModel.preferredArea.observe(fragment.viewLifecycleOwner) {
             areaSelectorMenuItem.title = fragment.getString(
                 R.string.area_selecting_action_selector,
                 it.displayName
             )
         }
 
-        areaSelectingViewModel.areas.observe(fragment.viewLifecycleOwner) { areas ->
+        viewModel.areas.observe(fragment.viewLifecycleOwner) { areas ->
             bd.button.setOnClickListener {
                 MaterialAlertDialogBuilder(fragment.requireContext())
                     .setTitle(R.string.area_selecting_action_selector_dialog_title)
-                    .setItems(areas.map { it.displayName }.toTypedArray()) { _, i ->
-                        areaSelectingViewModel.setPreferredAreaName(areas[i].name)
-                    }
+                    .setItems(areas.map { area ->
+                        if (viewModel.preferredArea.value == area) area.selectedDisplayName
+                        else area.displayName
+                    }.toTypedArray()) { _, i -> viewModel.setPreferredAreaName(areas[i].name) }
                     .show()
             }
         }
 
         areaSelectorMenuItem.setOnMenuItemClickListener { bd.button.callOnClick() }
-        launch { areaSelectingViewModel.updateAreas() }
+        launch { viewModel.updateAreas() }
     }
+
+    private val Area.selectedDisplayName
+        get() = getContext()?.run {
+            SpannableString(
+                getString(
+                    R.string.area_selecting_action_selector_dialog_current,
+                    displayName
+                )
+            ).apply { set(0..length, ForegroundColorSpan(getColor(R.color.inactive))) }
+        }
 }
